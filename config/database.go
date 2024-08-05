cc3c469bed52df11defcdd761948126808286b16
hiennq12 <hien.nq.hust@gmail.com>
2024-08-06 00:06:07 +0700

    connect postgres and config connect in file yaml

--- config/database.go
+++ config/database.go
@@ -1,1 +1,23 @@
 package config
+
+import (
+	"github.com/hiennq12/project_backend/cmd/struct_model"
+	"gopkg.in/yaml.v2"
+	"os"
+)
+
+func ReadConfigDb(configFile string) (*struct_model.ConfigDb, error) {
+	dataFile, err := os.ReadFile(configFile)
+	if err != nil {
+		//log.Fatal(fmt.Sprintf("Error when read file config database. Detail: [%v]"), err.Error())
+		return nil, err
+	}
+
+	dbConfig := &struct_model.ConfigDb{}
+	err = yaml.Unmarshal(dataFile, dbConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return dbConfig, nil
+}
