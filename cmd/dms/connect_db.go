ace41d3dde39f159865b9a0d9a2edff43fc36f94
hiennq12 <hien.nq.hust@gmail.com>
2024-08-07 23:45:48 +0700

    test func insert use squirrel build query

--- cmd/dms/connect_db.go
+++ cmd/dms/connect_db.go
@@ -1,14 +1,43 @@
 package dms
 
 import (
+	"database/sql"
 	"fmt"
-	"testing"
+	"github.com/hiennq12/project_backend/cmd/struct_model"
+	"github.com/hiennq12/project_backend/config"
+	_ "github.com/lib/pq"
+	"log"
 )
 
-func main() {
-	fmt.Println("daksjlkajsd")
-}
+var connectDB *sql.DB
 
-func TestConnectDbPostgreSQL(t *testing.T) {
+func ConnectDbPostgreSQL() (*sql.DB, error) {
+	dataConfig, err := config.ReadConfigDb("../../config/connect_database.yaml")
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Error when read file config database. Detail: [%v]", err.Error()))
+		return nil, err
+	}
+
+	if dataConfig == nil || dataConfig.Database == (struct_model.DatabaseConfig{}) { // check struct zero value
+		log.Fatal(fmt.Sprintf("Data config is empty"))
+		return nil, err
+	}
+
+	dataSource := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=%v",
+		dataConfig.Database.Host, dataConfig.Database.Port, dataConfig.Database.User,
+		dataConfig.Database.Password, dataConfig.Database.Dbname, dataConfig.Database.Sslmode)
+	connectPostgreSQL, err := sql.Open("postgres", dataSource)
+	if err != nil {
+		log.Fatalf(fmt.Sprintf("Error when connect to postgreSQL. Detail: [%v]", err.Error()))
+		return nil, err
+	}
+	//defer connectPostgreSQL.Close()
+
+	log.Printf("Connect database succcess!!")
+	return connectPostgreSQL, nil
+	//err = connectPostgreSQL.Ping()
+	//if err != nil {
+	//	log.Fatalf(fmt.Sprintf("Test ping db error. Detail: [%v]", err.Error()))
+	//}
 
 }
--- go.mod
+++ go.mod
@@ -6,9 +6,12 @@ require github.com/gofiber/fiber/v2 v2.52.5
 
 require (
 	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/Masterminds/squirrel v1.5.4 // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
